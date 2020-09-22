878210795ac1eb4ba1739fcfc2e0419711010a83
NestorMonroy <nestor.monroy.90@gmail.com>
2020-09-21 22:07:35 -0500

    add header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+      </Switch>
     </div>
   );
 }
