c54d6086e522d634df1994251073fab8fac5a691
NestorMonroy <nestor.monroy.90@gmail.com>
2020-09-23 19:28:42 -0500

    added addItem and removeItem and funtianlity

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,9 @@
 import CartActionTypes from './cart.types';
-import { addItemToCart } from './cart.utils'
+import { 
+    addItemToCart,
+    removeItemFromCart
+} from './cart.utils'
+
 const INITIAL_STATE = {
     hidden: true,
     cartItems: []
@@ -24,6 +28,13 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                     cartItem => cartItem.id !== action.payload.id
                 )
             };
+
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems:removeItemFromCart(state.cartItems, action.payload)
+            };
+        
         
         default:
             return state;
